62831ad2e9075dea2fa89ef0b05976180703d430
zhoujiamin33 <85230480+zhoujiamin33@users.noreply.github.com>
2021-07-12 16:51:08 +0800

    退住

--- src/components/cost/costlist.vue
+++ src/components/cost/costlist.vue
@@ -1,4 +1,12 @@
 <template>
+	<!-- 分页 -->
+	<div class="block">
+	    <span class="demonstration">调整每页显示条数</span>
+	    <el-pagination @size-change="handleSizeChange" background @current-change="handleCurrentChange"
+	      v-model:currentPage="currentPage2" :page-sizes="[100, 200, 300, 400]" :page-size="100"
+	      layout="sizes, prev, pager, next" :total="1000">
+	    </el-pagination>
+	  </div>
 </template>
 
 <script>
